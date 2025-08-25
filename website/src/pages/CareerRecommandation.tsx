542c0f56009d75c2f40c90eb286c209c06d64fff
nitinsharma450 <nitinsharma1059@gmail.com>
2025-08-25 15:55:08 +0530

    updated website code

--- website/src/pages/CareerRecommandation.tsx
+++ website/src/pages/CareerRecommandation.tsx
@@ -11,7 +11,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      let response = await fetch("https://career-backend-pi.vercel.app/api/user/search", {
+      let response = await fetch("https://career-backend-r0ysi3moh-nitinsharma1059-1842s-projects.vercel.app/api/user/search", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
