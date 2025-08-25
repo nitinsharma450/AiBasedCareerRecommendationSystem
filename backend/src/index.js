d89be85e6adaf57ece8f3850fa6b1bba1261e6fa
nitinsharma450 <nitinsharma1059@gmail.com>
2025-08-25 15:56:51 +0530

    updated on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 // CORS configuration
 app.use(cors({
     origin: 'https://vision-path-qdyd0179z-nitinsharma1059-1842s-projects.vercel.app', // your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
    
 }));
 
