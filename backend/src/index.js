1266fae4f9e6b0f1c3fdebc0468f0b360c791177
nitinsharma450 <nitinsharma1059@gmail.com>
2025-08-26 12:40:58 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,18 +6,12 @@ import { userRoutes } from './userRoutes.js';
 const app = express();
 
 // CORS configuration
-app.use(cors({
-    origin: 'https://vision-path-qdyd0179z-nitinsharma1059-1842s-projects.vercel.app', // your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-   
-}));
+app.use(cors());
 
 
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("✅ Backend is running on Render!");
-});
+
 
 // Your API routes
 app.use('/api', userRoutes);
